a81a0d14c320337096aa8712bd1f097cb551122b
hee_!J <heeej920@naver.com>
2025-02-02 16:16:11 +0900

    Fix: cors 에러 임시 수정

--- src/main/java/Spring/MindStone/web/controller/OnboardingController.java
+++ src/main/java/Spring/MindStone/web/controller/OnboardingController.java
@@ -1,6 +1,7 @@
 package Spring.MindStone.web.controller;
 
 
+import Spring.MindStone.config.jwt.JwtTokenUtil;
 import Spring.MindStone.domain.member.MemberInfo;
 import Spring.MindStone.service.onboardingService.OnboardingService;
 import Spring.MindStone.web.dto.onboardingDto.OnboardingRequestDto;
@@ -13,7 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "*")       // CORS 관련 설정
 @Tag(name = "Onboarding", description = "회원 온보딩 관련 API")
 @RestController
 @RequestMapping("/api/members")
@@ -31,9 +32,16 @@ public class OnboardingController {
             description = "회원이 온보딩 정보를 작성하면 유효성 검사를 거쳐 회원 정보를 저장합니다."
     )
     @PostMapping("/survey")
-    public OnboardingResponseDto onboardSurvey(@Valid @RequestBody OnboardingRequestDto requestDto) {
+    /*public OnboardingResponseDto onboardSurvey(@Valid @RequestBody OnboardingRequestDto requestDto) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String userEmail = ((MemberInfo) auth.getPrincipal()).getEmail();
+        return onboardingService.onboardMember(requestDto, userEmail);
+    }*/
+    public OnboardingResponseDto onboardSurvey(@Valid @RequestBody OnboardingRequestDto requestDto,
+                                               @RequestHeader("Authorization") String authorization) {
+
+        // JWT 토큰에서 이메일을 추출 (임시)
+        String userEmail = JwtTokenUtil.extractUserEmail(authorization);
         return onboardingService.onboardMember(requestDto, userEmail);
     }
 }
