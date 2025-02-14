b7c4300f78c970c0bbe64d34f80f93e40e20505b
wslee6704 <felix6704@cau.ac.kr>
2025-02-14 13:33:44 +0900

    fix : AiRecommandResponseDto 수정
    스웨거 상에서 안뜨길래 봤더니 어노테이션 안해놨음..

--- src/main/java/Spring/MindStone/web/dto/emotionWayDto/AiRecommandResponseDto.java
+++ src/main/java/Spring/MindStone/web/dto/emotionWayDto/AiRecommandResponseDto.java
@@ -1,8 +1,15 @@
 package Spring.MindStone.web.dto.emotionWayDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AiRecommandResponseDto {
     String recommand;
     String previousRecommand;
