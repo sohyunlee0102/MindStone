1c005d74c535d1e006cec61df70b5b6f5b0694e1
shlee <kwlee0100@naver.com>
2025-02-18 17:37:15 +0900

    Fix: Time Zone 한국 시간 변경

--- src/main/java/Spring/MindStone/config/TimeZoneConfig.java
+++ src/main/java/Spring/MindStone/config/TimeZoneConfig.java
@@ -8,7 +8,7 @@ import java.util.TimeZone;
 @Configuration
 public class TimeZoneConfig {
 
-    // 타임존 변경
+    // 타임존 변경2
     @PostConstruct
     public void init() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
